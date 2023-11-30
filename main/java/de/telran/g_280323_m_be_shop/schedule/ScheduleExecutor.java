3135d98ac29bf6b97ef06837dc985b331af1420e
SergejsP84 <129493689+SergejsP84@users.noreply.github.com>
2023-11-30 11:35:00 +0200

    Update ScheduleExecutor.java

--- main/java/de/telran/g_280323_m_be_shop/schedule/ScheduleExecutor.java
+++ main/java/de/telran/g_280323_m_be_shop/schedule/ScheduleExecutor.java
@@ -1,9 +1,13 @@
 package de.telran.g_280323_m_be_shop.schedule;
 
+import de.telran.g_280323_m_be_shop.domain.entity.interfaces.Product;
 import de.telran.g_280323_m_be_shop.domain.entity.jpa.Task;
+import de.telran.g_280323_m_be_shop.repository.jpa.TaskRepository;
+import de.telran.g_280323_m_be_shop.service.jpa.JpaProductService;
 import de.telran.g_280323_m_be_shop.service.jpa.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +17,11 @@ import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.time.Instant;
+import java.util.List;
+
+import static de.telran.g_280323_m_be_shop.constants.Constants.*;
 
 @Component
 @EnableScheduling
@@ -24,8 +32,47 @@ public class ScheduleExecutor {
 
     private TaskService service;
 
-    public ScheduleExecutor(TaskService service) {
+    private JpaProductService productService;
+
+    @Autowired
+    public ScheduleExecutor(TaskService service, JpaProductService productService)
+    {
+        this.productService = productService;
         this.service = service;
+    }
+
+    // Task 1
+    @Scheduled(fixedRate = 30000)
+    public void HomeworkTask1() {
+        try {
+            System.out.println("Latest tasks performed:");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `280323-m-be-shop-jpa`.task\n" +
+                    "ORDER BY executed_at DESC\n" +
+                    "LIMIT 5");
+            while (resultSet.next()) {
+                String description = resultSet.getString("description");
+                Timestamp executedAt = resultSet.getTimestamp("executed_at");
+                System.out.println("Task " + description + " performed at " + executedAt);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Task 2
+    // см. cron-interval в application.properties - cron-interval=15,45 * * * * *
+    @Scheduled(cron = "${cron-interval}")
+    public void HomeworkTask2() {
+        Task task = service.createTask("Obtained the last product added to the database");
+        LOGGER.info(task.getDescription());
+        List<Product> allProducts = productService.getAll();
+        Product lastProduct = allProducts.get(allProducts.size() - 1);
+        LOGGER.info("The last product added to the database is a " + lastProduct.getName());
     }
 
 //    @Scheduled(fixedDelay = 5000)
